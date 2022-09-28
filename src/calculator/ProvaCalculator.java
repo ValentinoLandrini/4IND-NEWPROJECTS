b71d0850d0352a43ad8e1cefff877230105bf1f9
valentino.landrini <valentino.landrini@AULA75-PC18.itib.local>
2022-09-28 09:45:56 +0200

    primo salvataggio

---
+++ src/calculator/ProvaCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+/**
+ *
+ * @author valentino.landrini
+ */
+public class ProvaCalculator {
+    
+    
+public static void main(String[] args) {
+    
+    
+    Calculator c = new Calculator();
+    
+    c.calc();
+    
+    }
+}
