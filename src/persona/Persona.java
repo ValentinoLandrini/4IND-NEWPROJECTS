4c8fe90e37db4dd9edc887d6201c3f5477dcac8e
valentino.landrini <valentino.landrini@AULA75-PC18.itib.local>
2022-09-28 10:20:46 +0200

    secondo salvataggio

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package persona;
 
 /**
@@ -10,5 +5,14 @@ package persona;
  * @author valentino.landrini
  */
 public class Persona {
+
+    public Double altezza;
+    public String cognome;
+    public String DataDiNascita;
+    public String nome;
+    public Float  peso;
+    
+    
+    
     
 }
