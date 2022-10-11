e0ea254e6aa2f9c9221ae2ab3ea480c44a7459ee
Valentino <114565901+ValentinoLandrini@users.noreply.github.com>
2022-10-11 14:36:58 +0200

    NON LO SO

--- src/persona/Persona2Prova.java
+++ src/persona/Persona2Prova.java
@@ -4,7 +4,7 @@ package persona;
  *
  * @author valentino.landrini
  */
-public class Persona1Prova {
+public class Persona2Prova {
 
     /**
      * @param args the command line arguments
@@ -12,7 +12,7 @@ public class Persona1Prova {
     public static void main(String[] args) {
         
         Persona1 p1 = new Persona1();
-        Persona2 p2 = new Persona2(1.80, "landrini", "19/08/2004", "valentino", 55f);
+        Persona2 p2 = new Persona2(1.80, "landrini", "19/8/2004", "valentino", 55f);
         
         String info = p2.info();
         
